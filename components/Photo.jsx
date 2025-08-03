08915ac7c286436a5ec753b66d58c4fd215be142
unknown <samiizadeh.ali@gmail.com>
2025-08-03 21:13:11 +0330

    stats added

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,57 +1,55 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
+
 const Photo = () => {
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-[300px] h-[300px] xl:w-[506px] xl:h-[506px]">
+      {/* Circular Clipped Image */}
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 0.4, delay: 2, ease: "easeIn" }}
+        transition={{ duration: 0.4, delay: 1, ease: "easeIn" }}
+        className="absolute inset-2 rounded-full overflow-hidden mix-blend-lighten"
       >
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.4, delay: 2, ease: "easeIn" }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
-        >
-          <Image
-            src="/assets/photo.png"
-            alt="ali samizade image"
-            priority
-            quality={100}
-            fill
-            className="object-contain"
-          />
-        </motion.div>
-
-        <motion.svg
-          className="w-[300px] h-[300px] xl:w-[506px] xl:h-[506px]"
-          fill="transparent"
-          viewBox="0 0 506 506"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <motion.circle
-            cx="253"
-            cy="253"
-            r="250"
-            stroke="#00ff99"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            initial={{ strokeDasharray: "24 10 0 0" }}
-            animate={{
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate: [120, 360],
-            }}
-            transition={{
-              duration: 20,
-              repeat: Infinity,
-              repeatType: "reverse",
-            }}
-          />
-        </motion.svg>
+        <Image
+          src="/assets/photo.jpg"
+          alt="ali samizade image"
+          priority
+          quality={100}
+          fill
+          className="object-cover"
+        />
       </motion.div>
+
+      {/* Animated SVG Circle Frame */}
+      <motion.svg
+        className="w-full h-full"
+        fill="transparent"
+        viewBox="0 0 506 506"
+        transition={{ delay: 1 }}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <motion.circle
+          cx="253"
+          cy="253"
+          r="250"
+          stroke="#00ff99"
+          strokeWidth="4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          initial={{ strokeDasharray: "24 10 0 0" }}
+          animate={{
+            strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
+            rotate: [120, 360],
+          }}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        />
+      </motion.svg>
     </div>
   );
 };
--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,9 +1,53 @@
+"use client";
+import CountUp from "react-countup";
 
-
+const stats = [
+  {
+    num: 3,
+    text: "Years Of Experience",
+  },
+  {
+    num: 14,
+    text: "Project Compeleted",
+  },
+  {
+    num: 8,
+    text: "Technologies Mastered",
+  },
+  {
+    num: 648,
+    text: "Code Commits",
+  },
+];
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <section>
+      <div className="container mx-auto">
+        <div className="flex flex-wrap max-w-[80vw] xl:max-w-none gap-6 mx-auto">
+          {stats.map((stat) => (
+            <div
+              key={stat.text}
+              className="flex flex-1 gap-4 items-center justify-center xl:justify-start"
+            >
+              <CountUp
+                end={stat.num}
+                duration={5}
+                delay={2}
+                className="text-4xl font-extrabold xl:text-6xl"
+              />
+              <p
+                className={`${
+                  stat.text.length > 15 ? "max-w-[100px]" : "max-w-[150px]"
+                } leading-snug text-white/80`}
+              >
+                {stat.text}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Stats
+export default Stats;
